35b8efce85af2ccf3a70d3d221f81e9226e66aa6
KiraCodingBot <helloworld.stephanie@gmail.com>
2022-03-03 01:23:09 -0500

    itemset class and apriori ready

--- Apriori.py
+++ Apriori.py
@@ -1,97 +1,12 @@
-import collections as col
-
-features=[' workclass', ' marital-status', ' occupation', ' relationship', ' race', ' sex', ' native-country', ' education']
-MIN_SUP=3000
-
-class Itemsets:
-
-    def __init__(self, k):
-        self.k=k
-        self.freq_items=[]
-        
-    
-
-def find_1_item(data):
-    count=[]
-    l1=[{x} for x in range(8)]
-    for f in features:
-        c=col.Counter(data[f])
-        dic=dict(c)
-        dic={frozenset([key]):val for key, val in dic.items() if val>= MIN_SUP and key!=' ?'}
-        count.append(dic)
-    return count, l1
+import itemset as it
 
 def apriori(data):
     result=[]
-
-
-def apriori_gen(l, k):
-    # l is L_K-1 
-    C_k=[]
-    for i in range(len(l)):
-        item1=l[i]
-        for j in range(i+1, len(l)):
-            item1_lst=list(item1)
-            item2=l[j]
-            item2_lst=list(item2)
-            if item1_lst[:k-2] == item2_lst[:k-2] and item1_lst[-1]<item2_lst[-1]:
-                item1_lst.append(item2_lst[-1])
-                c=set(item1_lst)
-                if not has_infrequent_subset(l, c):
-                    C_k.append(c)
-    return C_k
-                
-
-
-def has_infrequent_subset(l, c):
-    c_lst=list(c)
-    for i in range(len(c_lst)):
-        c_copy=c_lst.copy()
-        c_copy.pop(i)
-        temp=set(c_copy)
-        if temp not in l:
-            return True
-    return False
-
-            
-
-
-def find_2_item(data, one_item, min_sup=3000):
-    feature_2=[]
-    for i in  range(8):
-        temp=set()
-        temp.add(i)
-        for j in range(i+1,8):  
-            temp.add(j)
-            feature_2.append(temp)
-            temp={i}
-    print(feature_2)
-    count=[]
-    for f in feature_2:
-        temp=[]
-        a, b = list(f)[0], list(f)[1]
-        item1, item2 = one_item[a], one_item[b]
-        dic={}
-        for x in item1.keys():
-            for y in item2.keys():
-                key=x.union(y)
-                dic[key]=0
-        for _, point in data.iterrows():
-            for key in dic.keys():
-                label=list(key)
-                if point[features[a]]==label[1] and point[features[b]]==label[0]:
-                    dic[key]+=1
-        dic={key: val for key, val in dic.items() if val>=min_sup}
-        if dic:
-            temp.append(dic)
-            count.append((f, temp))
-    print(len(count))
-    print(count)
-
-    
-
-
-
-
-
+    itemsets1=it.Itemsets(1, data)
+    result.append(itemsets1)
+    for i in range(2,9):
+        prev=result[-1].deepcopy()
+        itemsets=it.Itemsets(i, l_prev=prev.l)
+        
+        
 
--- main.py
+++ main.py
@@ -1,66 +1,20 @@
 import pandas as pd
 import configuration as config
-import Apriori as ap
+import itemset as it
     
 
 def main():
     data =  pd.read_csv(config.FILE, sep=",")
-    '''
-    #age
-    sim.numeric(data, 4, 5, 'age')
     
-    #fnlwgt
-    sim.numeric(data, 4, 5, ' fnlwgt')
-
-    #education-num
-    sim.numeric(data, 4, 5, ' education-num')
-   
-    #capital-gain
-    sim.numeric(data, 4, 5, ' capital-gain')
-
-    #capital-loss
-    sim.numeric(data, 4, 5, ' capital-loss')
-
-    #hours-per-week
-    sim.numeric(data, 4, 5, ' hours-per-week')
-    
-    
-    
-    #workclass
-    sim.nominal(data, 4, 5, ' workclass')
-    
-    #marital-status
-    sim.nominal(data, 4, 5, ' marital-status')
-
-    #occupation
-    sim.nominal(data, 4, 5, ' occupation')
-    
-    #relationship
-    sim.nominal(data, 4, 5, ' relationship')
-    
-    #race
-    sim.nominal(data, 4, 5, ' relationship')
-
-    #sex
-    sim.nominal(data, 4, 5, ' sex')
-    
-    #native-country
-    sim.nominal(data, 4, 5, ' native-country')
-    
-
-    
-    
-    #education
-    sim.ordinal(data, 4, 5, ' education')
-    
-    '''
     '''
     one_item=ap.find_1_item(data)
     count=ap.find_2_item(data, one_item)
     '''
-    l=[{1,2}, {1,3}, {1,5}, {2, 3}, {2, 4}, {2, 5}]
-    C_k=ap.apriori_gen(l, 3)
-    print(C_k)
+    itemsets1=it.Itemsets(1, data)
+    print(itemsets1.freq_items)
+    print(itemsets1.l)
+    itemsets2=it.Itemsets(2, l_prev=itemsets1.l)
+    print(itemsets2.l)
    
    
     
