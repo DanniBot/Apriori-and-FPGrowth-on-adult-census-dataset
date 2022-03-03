2aebe4d947ad59a45cc699e4814b9e88a869081b
funes <zwang164@ur.rochester.edu>
2022-03-02 21:57:44 -0500

    first commit

---
+++ Apriori.py
@@ -0,0 +1,69 @@
+import collections as col
+
+features=[' workclass', ' marital-status', ' occupation', ' relationship', ' race', ' sex', ' native-country', ' education']
+
+def find_1_item(data, min_sup=3000):
+    count=[]
+    for f in features:
+        c=col.Counter(data[f])
+        dic=dict(c)
+        dic={frozenset([key]):val for key, val in dic.items() if val>= min_sup and key!=' ?'}
+        count.append(dic)
+    return count
+
+
+def apriori_gen(l, k):
+    # l is L_K-1 
+    l_copy=l
+    for i in range(len(l)):
+        item1=l[i]
+        for j in range(i+1, len(l)):
+            item2=l[j]
+            if list(item1)[:k-1] == list(item2)[:k-1] and 
+
+            
+
+
+def find_2_item(data, one_item, min_sup=3000):
+    feature_2=[]
+    for i in  range(8):
+        temp=set()
+        temp.add(i)
+        for j in range(i+1,8):  
+            temp.add(j)
+            feature_2.append(temp)
+            temp={i}
+    print(feature_2)
+    count=[]
+    for f in feature_2:
+        temp=[]
+        a, b = list(f)[0], list(f)[1]
+        item1, item2 = one_item[a], one_item[b]
+        dic={}
+        for x in item1.keys():
+            for y in item2.keys():
+                key=x.union(y)
+                dic[key]=0
+        for _, point in data.iterrows():
+            for key in dic.keys():
+                label=list(key)
+                if point[features[a]]==label[1] and point[features[b]]==label[0]:
+                    dic[key]+=1
+        dic={key: val for key, val in dic.items() if val>=min_sup}
+        if dic:
+            temp.append(dic)
+            count.append((f, temp))
+    print(len(count))
+    print(count)
+
+    
+
+
+
+
+'''
+def apriori(data, min_sup=2000):
+    one_item=find_1_item(data, min_sup)
+'''
+
+
---
+++ main.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import configuration as config
+import Apriori as ap
+    
+
+def main():
+    data =  pd.read_csv(config.FILE, sep=",")
+    '''
+    #age
+    sim.numeric(data, 4, 5, 'age')
+    
+    #fnlwgt
+    sim.numeric(data, 4, 5, ' fnlwgt')
+
+    #education-num
+    sim.numeric(data, 4, 5, ' education-num')
+   
+    #capital-gain
+    sim.numeric(data, 4, 5, ' capital-gain')
+
+    #capital-loss
+    sim.numeric(data, 4, 5, ' capital-loss')
+
+    #hours-per-week
+    sim.numeric(data, 4, 5, ' hours-per-week')
+    
+    
+    
+    #workclass
+    sim.nominal(data, 4, 5, ' workclass')
+    
+    #marital-status
+    sim.nominal(data, 4, 5, ' marital-status')
+
+    #occupation
+    sim.nominal(data, 4, 5, ' occupation')
+    
+    #relationship
+    sim.nominal(data, 4, 5, ' relationship')
+    
+    #race
+    sim.nominal(data, 4, 5, ' relationship')
+
+    #sex
+    sim.nominal(data, 4, 5, ' sex')
+    
+    #native-country
+    sim.nominal(data, 4, 5, ' native-country')
+    
+
+    
+    
+    #education
+    sim.ordinal(data, 4, 5, ' education')
+    
+    '''
+
+    one_item=ap.find_1_item(data)
+    count=ap.find_2_item(data, one_item)
+    
+   
+    
+
+
+if __name__ == '__main__':
+    main()
